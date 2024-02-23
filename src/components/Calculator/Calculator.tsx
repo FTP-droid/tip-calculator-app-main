127c80a0b641eb64674a3a3af40265195f8b8708
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2024-02-23 04:23:55 -0500

    Added bill input in Calculator component and styled the bill input with a label and css according to the given template.

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,13 +1,20 @@
 import styles from './Calculator.module.css';
+import dollarIcon from '../../../images/icon-dollar.svg';
 
 function Calculator() {
   return (
     <main className={styles.container}>
-      <section>
-        InputCard
+      <section className={styles['card']}>
+        <div id={styles['bill-input-container']}>
+          <label htmlFor='bill-input' id={styles['bill-input-label']}>Bill</label>
+          <div id={styles['bill-input-wrapper']}>
+            <input id={styles['bill-input']} placeholder='0'/>
+            <img src={dollarIcon} className={styles.icon}/>
+          </div>
+        </div>
       </section>
-      <section>
-        InputCard
+      <section className={styles['card']}>
+        Output
       </section>
     </main>
   )
