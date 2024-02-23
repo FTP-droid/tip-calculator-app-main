8308a4d7d93328153e32f23f5fbd6520e90b3113
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2024-02-23 04:59:54 -0500

    In index.css, set default font-family and font-weight to a variable to be used throughout the application. In Calculator component, added basic to be styled set-tip-percentage section.

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -2,18 +2,27 @@ import styles from './Calculator.module.css';
 import dollarIcon from '../../../images/icon-dollar.svg';
 
 function Calculator() {
+  const tipPercentages = [5, 10, 15, 25 , 50];
+
   return (
     <main className={styles.container}>
       <section className={styles['card']}>
+        {/* Input for the bill */}
         <div className={styles['input-container-with-label']}>
           <label htmlFor='bill-input' className={styles['input-label']}>Bill</label>
           <div id={styles['bill-input-wrapper']}>
             <input id={styles['bill-input']} placeholder='0'/>
             <img src={dollarIcon} className={styles.icon}/>
           </div>
-          <div className={styles['input-container-with-label']}>
-            <label htmlFor='select-tip-input' className={'input-label'}></label>
-          </div>
+        </div>
+        {/* Input for the tip percentage */}
+        <div className={styles['input-container-with-label']}>
+            <div className={styles['input-label']}> Select Tip %</div>
+            <div id={styles['tip-percentage-container']}>
+              {tipPercentages.map(e => 
+                <button className={styles['tip-percentage']}>{e}%</button>
+              )}
+            </div>
         </div>
       </section>
       <section className={styles['card']}>
